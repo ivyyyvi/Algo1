bc0ca4622e75fcc8fadd57851614f09ca8491406
Ivy <ivy@Ivys-iMac.local>
2016-08-15 00:08:13 +0800

    Initial Dijkstra's skeleton

--- Dijkstra/main.c
+++ Dijkstra/main.c
@@ -164,7 +164,18 @@ void
 Dijkstra (int *A, int num_nodes, edge* E, int num_edges)
 {
   int sourceVertex = 1;
-  A[sourceVertex] = 0;
+  int *X; // vertices processed so far
+
+  X = calloc (num_nodes + 1, sizeof (int)); // 1-base
+  X[0] = sourceVertex;
+  A[sourceVertex] = 0; // 1-base
+
+  //
+  // Among all edges (v, w) in E
+  // with v in X and w not in X,
+  // pick the one that minimizes A[v] + l(v,w)
+  //
+
 }
 
 int main ()
